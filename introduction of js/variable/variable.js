17ea727dcce3b8ac91783262eec64319751f0e45
Emdadul <you@example.com>
2025-12-23 21:21:09 +0600

    if statement 1st

--- introduction of js/variable/variable.js
+++ introduction of js/variable/variable.js
@@ -2,6 +2,50 @@
 5 things to declear a variable
 */
 
+/*
+// 1. no keyword in varibale name 
+var var = "hello"; // invalid
+var false = "hello"; // invalid
+
+// 2. no space in variable name
+var my name = "talha"; // invalid
+var my address = "dahka"; // invalid
+
+// 3. no quotation mark in variable name
+var "myname" = "talha"; // invalid
+var 'myaddress' = "dahka"; // invalid
+
+// 4. can't start with a number 
+var 2name = "talha"; // invalid
+var 3address = "dahka"; // invalid
+
+// 5. name is case sensitive
+var myName = "talha"; // valid
+var Myname = "talha"; // valid
+var MYNAME = "talha"; // valid
+
+// 6. how to write long variable names
+var my_full_name = "talha"; // snake case
+var myFullName = "talha"; // camel case
+var MyFullName = "talha"; // pascal case 
+*/
+
+/**
+ * COMPARISON
+ * 1. bigger: >
+ * 2. smaller: <
+ * 3. equal: ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 7. strict equal: ===
+ * 8. strict not equal: !==
+ * 
+ * ------------------------------
+ * 7. and: &&
+ * 8. or: ||
+*/
+
 //------------chapter1------------------
 /* Number */
 var age = 45 ; 
@@ -64,37 +108,25 @@ sunglass_price += 200; //2nd way, 2nd update 700 + 200 = 900
 console.log(sunglass_price);
 //------------------------------
 
+//-------------chapter4----------------
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 5);
+console.log(5 >= 8);
+console.log(5 <= 2);
+console.log(5 != 1);
+console.log(5 == '5'); //true
+console.log(5 === '5'); //false
+//------------------------------
 
 
 
 
 
-/*
-// 1. no keyword in varibale name 
-var var = "hello"; // invalid
-var false = "hello"; // invalid
 
-// 2. no space in variable name
-var my name = "talha"; // invalid
-var my address = "dahka"; // invalid
 
-// 3. no quotation mark in variable name
-var "myname" = "talha"; // invalid
-var 'myaddress' = "dahka"; // invalid
 
-// 4. can't start with a number 
-var 2name = "talha"; // invalid
-var 3address = "dahka"; // invalid
 
-// 5. name is case sensitive
-var myName = "talha"; // valid
-var Myname = "talha"; // valid
-var MYNAME = "talha"; // valid
 
-// 6. how to write long variable names
-var my_full_name = "talha"; // snake case
-var myFullName = "talha"; // camel case
-var MyFullName = "talha"; // pascal case 
-*/
 
 
--- introduction of js/variable_practice/variable_P.js
+++ introduction of js/variable_practice/variable_P.js
@@ -21,7 +21,56 @@ var talhaisaboy = false;
 var talhaisaman = true;
 console.log(talhaisaman);
 
-var pen_price = -s10;
-var pencil_price = 0;
+var pen_price = 10;
+var pencil_price = 2;
 var total_price = pen_price / pencil_price;
 console.log(total_price.toFixed(2));
+
+
+var orangenumber = Number('20');
+console.log(orangenumber);
+var user = null;
+var age;
+console.log(age);
+console.log(user);
+
+var fishname = "500fish";
+console.log(isNaN(fishname));
+//-----------------------------------------------
+
+
+
+
+//--------------practice task-1------------------
+
+var balance = 1000;
+var orangesPrice = 400;
+var applesPrice = 300;
+var totalFoodPrice = orangesPrice + applesPrice;
+var remainingBalance = balance - totalFoodPrice;
+console.log(remainingBalance);
+
+//--------------practice task-2------------------
+
+var bangla = 80;
+var math = 85.5;
+var biology = 70;
+var physics = 75.5;
+var chemistry = 65;
+var totalmarks = bangla + math + biology + physics + chemistry;
+var averagemarks = totalmarks / 5;
+console.log(averagemarks.toFixed(2));
+
+//--------------practice task-3------------------
+var bus = 122;
+bus %= 4;
+console.log(bus);
+
+//--------------practice task-4------------------
+
+const phone = isNaN("11");
+console.log(phone);
+const laptop = isNaN(2-10);
+console.log(laptop);
+
+//--------------practice task-5------------------
