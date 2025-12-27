be0d647d1c337dd70110716fe37166f6cd4aa675
Emdadul <you@example.com>
2025-12-27 23:35:23 +0600

    for-loop 1st

--- introduction of js/JS-loop/index.js
+++ introduction of js/JS-loop/index.js
@@ -14,5 +14,48 @@ for (const food of dish){
 let i = 1;
 while (i <= 5){
     console.log(i);
-    i++ ;
+    i += 1 ;
 }
+
+//--------------------sum-loop--------------------
+let num1 = 2 ;
+let sum = 2;
+
+while(num1 <= 5){
+    sum = sum + num1 ;
+    console.log(sum);
+    num1++;
+}
+
+//-------------------use if else in loop----------
+let x = 2;
+
+while(x <= 10){
+    console.log(x);
+
+    if (x % 2 === 0){
+        console.log("even number", x);
+    }
+
+    x++;
+}
+
+//----------------------------for-loop--------------------------------
+let sum2 = 0;
+for (let num2 = 11; num2 <= 20; num2 ++){
+    sum2 = sum2 + num2;
+}
+
+console.log('sum of number  11 to 20 is', sum2);
+
+//------------------------incremental-for-loop--------------------------
+for(let age = 1; age <= 10; age++){
+    console.log(age);
+}
+
+//------------------------decremental-for-loop--------------------------
+
+for(let age2 = 10; age2 >= 0; age2--){
+    console.log(age2);
+}
+
---
+++ introduction of js/JS-loop_task/index.js
@@ -0,0 +1,57 @@
+//-------------------task-1-(while-loop)--------------------
+let x = 1;
+while (x <= 5){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    x++ ;
+}
+
+//-------------------subtask-1-(while-loop)--------------------
+let num1 = 61;
+
+while (num1 <= 100){
+    // console.log(num1);
+
+    if (num1 % 2 === 1){
+        console.log('this is a odd number', num1);
+    }
+    num1++ ;
+}
+
+//-------------------subtask-1-(while-loop)--------------------
+let num2 = 78;
+
+while (num2 <= 98){
+    
+    if (num2 % 2 == 0){
+        console.log('this is a even number', num2);
+    }
+
+    num2++ ;
+}
+
+//-------------------subtask-3-(while-loop)--------------------
+let num3 = 81;
+let sum1 = 0;
+
+while(num3 <= 131){
+    if(num3 % 2 == 1){
+        sum1 = sum1 + num3 ; 
+        console.log(sum1); 
+    }
+
+    num3++ ;
+}
+
+//-------------------subtask-4-(while-loop)--------------------
+let num4 = 206;
+let sum2 = 0;
+
+while(num4 <= 311){
+    if(num4 % 2 == 0){
+        sum2 = sum2 + num4 ; 
+        console.log(sum2); 
+    }
+
+    num4++ ;
+}
+
