6dbe452d412e41dcd87d9646c44f3d8d398d167a
Emdadul <you@example.com>
2026-01-02 00:20:48 +0600

    js-object task complate

---
+++ introduction of js/object_task/index.js
@@ -0,0 +1,52 @@
+//----------------task 1----------------//
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+// console.log(colors['golden rod']);
+
+//----------------task 2----------------//
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    "passenger capacity": 5
+};
+
+//----------------task 3----------------//
+const uniStudent = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+// console.log(student.physics.marks);
+
+//----------------task 4---------------//
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const x = Object.keys(student);
+// console.log(x.length);
+
+//----------------task 5---------------//
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+for(const b in myObject){
+    // console.log('key:', b, '|', 'typr:', typeof myObject[b] );
+}
+
